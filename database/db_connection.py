7d2ca4f89963acf0f7320b9cb7ba66db4d9edaca
Abaasa Albert <164334496+Altech001@users.noreply.github.com>
2025-04-16 19:16:01 +0000

    db_update

--- database/db_connection.py
+++ database/db_connection.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:altech@001@localhost:5432/luco"
 
-# DB_URI = "postgresql://postgres:qhqDRvTxtUHJLcjHpJwvLOMuGGsospoO@shuttle.proxy.rlwy.net:29918/railway"
-DB_URI = "postgresql://postgres:OdwXQZSyHrQmapKKPQzviNIsuDfLiOhv@hopper.proxy.rlwy.net:32046/railway"
+# DB_URI = "postgresql://altech:DJ2ef3UJY1MnJYivE5fsPTNtjz9KYske@dpg-d0002ba4d50c739pjvqg-a/luco_sms"
+DB_URI = "postgresql://altech:DJ2ef3UJY1MnJYivE5fsPTNtjz9KYske@dpg-d0002ba4d50c739pjvqg-a.virginia-postgres.render.com/luco_sms"
 engine = create_engine(DB_URI, connect_args={"sslmode": "require"})
 
 
