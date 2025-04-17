1c26311d393d4f85cf4b8f3ad8f569ebfc2e964e
Abaasa Albert <164334496+Altech001@users.noreply.github.com>
2025-04-17 21:17:08 +0000

    file update

--- luco/sms_send.py
+++ luco/sms_send.py
@@ -1,24 +1,3 @@
-# import africastalking
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-
-# SANDBOX_API_KEY = os.getenv("SANDBOX_API_KEY")
-
-# class LucoSMS:
-#     def __init__(self, api_key=SANDBOX_API_KEY, username='sandbox', sender_id="ddddd"):
-#         africastalking.initialize(username=username, api_key=api_key)
-#         self.sender_id = sender_id
-#         self.sms = africastalking.SMS
-
-#     def send_message(self, message, recipients):
-#         try:
-#             response = self.sms.send(message, recipients, self.sender_id)
-#             return response
-#         except Exception as e:
-#             raise Exception(f"Failed to send SMS: {str(e)}")
-
 import africastalking
 from dotenv import load_dotenv
 import os
